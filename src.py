db33f155b9abc972fb618d1562435e4eda2bc45f
Chan C <1156262168@qq.com>
2021-11-22 18:40:19 +0800

    方案1：大图像写字，再resize，效果不好

--- src.py
+++ src.py
@@ -25,13 +25,36 @@ class TextProcesser:
             plt.show()
             print(np.array(image, dtype=bool))  # 转数组
         # 获取roi
-        image, w_h = self._get_roi(image)
+        image = self._get_roi(image)
+        if self.debug:
+            plt.imshow(image)  # 使用matplotlib显示
+            plt.show()
+            print(np.array(image, dtype=bool))  # 转数组
         # 调整分辨率
-        image = image.resize(self.col_num)
+        new_size = (int(self.col_num * image.size[0] / image.size[1]) + 1, self.col_num)
+        image = image.resize(new_size)
+        if self.debug:
+            plt.imshow(image)  # 使用matplotlib显示
+            plt.show()
+            print(np.array(image, dtype=bool))  # 转数组
         return image
 
-    def _get_roi(self, image):
+    def _get_roi(self, image0):
         """获取图像的文字区域roi"""
+        image = np.array(image0, dtype=bool)
+        roi = [-1, -1, -1, -1]
+        for i in range(image.shape[0] - 1):
+            # 行
+            if roi[0] == -1 and image[i, :].all() and not image[i + 1, :].all():
+                roi[0] = i - 1
+            elif not image[i, :].all() and image[i + 1, :].all():
+                roi[1] = i + 2
+            # 列
+            if roi[2] == -1 and image[:, i].all() and not image[:, i + 1].all():
+                roi[2] = i - 1
+            elif not image[:, i].all() and image[:, i + 1].all():
+                roi[3] = i + 2
+        return image0.crop((roi[2], roi[0], roi[3], roi[1]))
 
     def process(self, text):
         """将文本处理为emoji字符串"""
@@ -46,4 +69,4 @@ class TextProcesser:
 
 if __name__ == '__main__':
     tp = TextProcesser()
-    print(tp.process("这是一个你爱中文的我吴青ILOVEYOU"))
+    print(tp.process("ILOVEYOU"))
