27a4fac2907fb895c3e5a3cc706905aefc33e9b4
Chan C <1156262168@qq.com>
2021-11-22 18:01:38 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QCursor
+from PyQt5.QtCore import Qt
+
+import src
+
+UI_PATH = "ui.ui"
+
+
+class Main(QWidget):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        # 加载UI
+        self.main_win = uic.loadUi(UI_PATH)
+        # 连接按钮
+        self.main_win.runButton.clicked.connect(self._run)
+
+    def _run(self):
+        # 改变鼠标指针样式
+        self.main_win.setCursor(QCursor(Qt.WaitCursor))
+        # 处理
+        self.emojis = src.process(self.main_win.inputText.toPlainText())
+        # 输出结果
+        self.main_win.outputText.setPlainText(self.emojis)
+        # 改变鼠标指针样式
+        self.main_win.setCursor(QCursor(Qt.ArrowCursor))
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    main = Main()
+    main.main_win.show()
+    app.exec_()
---
+++ src.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+
+class TextProcesser:
+    def __init__(self, back='😘', fore='❤️', col_num=12):
+        self.background = back  # 背景表情
+        self.foreground = fore  # 前景表情
+        self.col_num = col_num  # 微信-11，qq-12
+        self.debug = True  # 是否为调试模式
+        self._image_size = (256, 256)  # 绘制文字的图像尺寸
+        self._font_size = 210  # 文字字体大小
+
+    def _draw_word(self, word):
+        """将字符绘制为图像"""
+        # 预设合适的字体，对于中文尤其重要，否则会乱码，这里使用常见的黑体
+        font = ImageFont.truetype("simhei", self._font_size, encoding='utf-8')
+        # 灰度图
+        image = np.zeros(self._image_size, dtype=np.uint8)
+        # 写字
+        image = cv2.putText(image, word, (0, 0), cv2.FONT_HERSHEY_SIMPLEX, 0.75, 0, 2)
+        if self.debug:
+            plt.imshow(image)  # 使用matplotlib显示
+            plt.show()
+            print(np.array(image, dtype=bool))  # 转数组
+        # 获取roi
+        image, w_h = self._get_roi(image)
+        # 调整分辨率
+        image = image.resize(self.col_num)
+        return image
+
+    def _get_roi(self, image):
+        """获取图像的文字区域roi"""
+
+    def process(self, text):
+        """将文本处理为emoji字符串"""
+        emojis = ""
+        # 遍历文字
+        for word in text:
+            # 画图
+            image = self._draw_word(word)
+
+        return emojis
+
+
+if __name__ == '__main__':
+    tp = TextProcesser()
+    print(tp.process("这是一个你爱中文的我吴青ILOVEYOU"))
